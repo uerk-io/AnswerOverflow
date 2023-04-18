d3f551ab0abef0a6c73c922b8cbd4be8881fa10c
uerk-io <38047633+uerk-io@users.noreply.github.com>
2023-04-17 23:25:16 -0400

    Add: Flow for Enable Auto Thread Menu

--- apps/discord-bot/src/components/settings/quick-start-menu.tsx
+++ apps/discord-bot/src/components/settings/quick-start-menu.tsx
@@ -8,7 +8,7 @@ import {
 } from '~discord-bot/components/primitives';
 import type { RootChannel } from '~discord-bot/utils/utils';
 import {
-	// ALLOWED_AUTO_THREAD_CHANNEL_TYPES,
+	ALLOWED_AUTO_THREAD_CHANNEL_TYPES,
 	OPEN_QUICK_START_MENU_LABEL,
 } from '@answeroverflow/constants';
 import {
@@ -17,65 +17,66 @@ import {
 	// ChannelSettingsMenuItemProps,
 	// updateChannelState,
 	ToggleIndexingButton,
-	// ToggleAutoThreadButton,
+	ToggleAutoThreadButton,
+	ToggleMarkAsSolutionButton,
 } from './channel-settings-menu';
 import LRUCache from 'lru-cache';
 
 // import { ChannelType } from 'discord.js';
 import type { ChannelWithFlags } from '@answeroverflow/prisma-types';
 // import type { Message } from '@answeroverflow/db';
-// import type { NewsChannel, TextChannel } from 'discord.js';
+import type { NewsChannel, TextChannel } from 'discord.js';
 
 const channelCache = new LRUCache<string, ChannelWithFlags>({
 	max: 500,
 	ttl: 1000 * 60 * 5,
 });
 
-// export function QuickStartEnableAutoThreadMenu({
-// 	targetChannel,
-// 	initialChannelData,
-// }: QuickStartMenuProps) {
-// 	const [channel, setChannel] = React.useState<ChannelWithFlags>(
-// 		channelCache.get(targetChannel.id) ?? initialChannelData,
-// 	);
-// 	//TODO: Use this for the Forum Consent Policy
-// 	// const isButtonInForumChannel = targetChannel.type === ChannelType.GuildForum;
-// 	return (
-// 		<>
-// 			{/* Flow for if  AutoThreading is Available*/}
-// 			{ALLOWED_AUTO_THREAD_CHANNEL_TYPES.has(targetChannel.type) && (
-// 				<>
-// 					<InstructionsContainer>
-// 						** Quick Config - Auto Threading ✅**
-// 						<Spacer count={2} />
-// 						<>
-// 							This channel is eligible for auto-threading, do you want to enable
-// 							it?
-// 						</>
-// 					</InstructionsContainer>
-// 					<ToggleAutoThreadButton
-// 						channelInDB={channel}
-// 						setChannel={setChannel}
-// 						targetChannel={targetChannel as TextChannel | NewsChannel}
-// 					/>
-// 				</>
-// 			)}
-// 			{/* Flow for if  AutoThreading is Available*/}
-// 			{ALLOWED_AUTO_THREAD_CHANNEL_TYPES.has(targetChannel.type) && (
-// 				<>
-// 					<InstructionsContainer>
-// 						** Quick Config - Auto Threading ❌**
-// 						<Spacer count={2} />
-// 						<>
-// 							This channel is not eligible for auto-threading, so let's move to
-// 							the next step.{' '}
-// 						</>
-// 					</InstructionsContainer>
-// 				</>
-// 			)}
-// 		</>
-// 	);
-// }
+export function QuickStartEnableAutoThreadMenu({
+	targetChannel,
+	initialChannelData,
+}: ChannelSettingsSubMenuProps) {
+	const [channel, setChannel] = React.useState<ChannelWithFlags>(
+		channelCache.get(targetChannel.id) ?? initialChannelData,
+	);
+	//TODO: Use this for the Forum Consent Policy and Enable Solution Tagging
+	// const isButtonInForumChannel = targetChannel.type === ChannelType.GuildForum;
+	return (
+		<>
+			{/* Flow for if  AutoThreading is Available*/}
+			{ALLOWED_AUTO_THREAD_CHANNEL_TYPES.has(targetChannel.type) && (
+				<>
+					<InstructionsContainer>
+						** Quick Config - Auto Threading ✅**
+						<Spacer count={2} />
+						<>
+							This channel is eligible for auto-threading, do you want to enable
+							it?
+						</>
+					</InstructionsContainer>
+					<ToggleAutoThreadButton
+						channelInDB={channel}
+						setChannel={setChannel}
+						targetChannel={targetChannel as TextChannel | NewsChannel}
+					/>
+				</>
+			)}
+			{/* Flow for if  AutoThreading is Available*/}
+			{ALLOWED_AUTO_THREAD_CHANNEL_TYPES.has(targetChannel.type) && (
+				<>
+					<InstructionsContainer>
+						** Quick Config - Auto Threading ❌**
+						<Spacer count={2} />
+						<>
+							This channel is not eligible for auto-threading, so let's move to
+							the next step.{' '}
+						</>
+					</InstructionsContainer>
+				</>
+			)}
+		</>
+	);
+}
 
 export function QuickStartEnableIndexingMenu({
 	targetChannel,
